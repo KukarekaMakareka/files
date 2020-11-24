b84fa08a2a759add8b0a5fc572778729269a76cd
Polina Fokina <fpa0512@gmail.com>
2020-11-24 17:27:50 +0300

    Read from text file

--- ProjectX.cpp
+++ ProjectX.cpp
@@ -6,12 +6,19 @@ using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "RUS");
+    /*setlocale(LC_ALL, "RUS");
     ofstream outfile;
     outfile.open("my_file.txt");
     string str;
     getline(cin, str);
     outfile << str;
-    outfile.close();
+    outfile.close();*/
+    //==================================================================
+    ifstream infile;
+    infile.open("my_file.txt");
+    string str1;
+    getline(infile, str1);
+    cout << str1 << endl;
+    infile.close();
     return 0;
 }
