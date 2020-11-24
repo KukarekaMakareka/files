9e65e6108d29d29fc366239bb57bd3e37022d4fd
Polina Fokina <fpa0512@gmail.com>
2020-11-24 17:18:49 +0300

    Write to text file

--- ProjectX.cpp
+++ ProjectX.cpp
@@ -1,11 +1,17 @@
 ﻿//Имя.расширение
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 int main()
 {
     setlocale(LC_ALL, "RUS");
-    cout << "Hello World!" << endl;
+    ofstream outfile;
+    outfile.open("my_file.txt");
+    string str;
+    getline(cin, str);
+    outfile << str;
+    outfile.close();
     return 0;
 }
