9b97893e51cb4d602b908fb3d6f417f2488f5484
Polina Fokina <fpa0512@gmail.com>
2020-11-24 16:59:04 +0300

    1st commit

---
+++ ProjectX.cpp
@@ -0,0 +1,11 @@
+﻿//Имя.расширение
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    cout << "Hello World!" << endl;
+    return 0;
+}
